8f0c831ae36a5831a45c1d5cb14789082439eec2
codemon99 <spectre1c@gmail.com>
2022-01-03 12:02:17 +0900

    memozi-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [textlen, setTextLen] = useState(0);
+
+  const handleTextArea = (e) => {
+    setTextLen(e.target.value.length);
+    
+  }
+
+  const handleChecked = (e) => {
+    console.log(e.target.checked);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='memozi-header'>
+        <label className='memozi-header__label'>{textlen}</label>
+        <div className='memozi-header__option'>
+          <label className='header__option_label'>띄어쓰기 빼기</label>
+          <input type="checkbox" className='memozi-header__option__blank' onChange={handleChecked}></input>
+        </div>
+      </div>
+      <textarea className='memozi-textarea' onChange={handleTextArea}></textarea>
     </div>
   );
 }
